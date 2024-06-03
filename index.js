070749404f4b3c4b88a0b8c1466ed9d43de01d1e
Abubakar Rasheed <131538521+abubakarrasheed@users.noreply.github.com>
2024-06-03 22:49:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ ________________________//Arrays in js
 
 let marks= [45,66,34,12,89];
 console.log(marks);
+
 console.log(marks.length);  //property
 console.log(marks[0]);// to access single item
 
